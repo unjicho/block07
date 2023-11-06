0e01c826133c9751d9b3085da711a3273eba3757
Unji Cho <unjicho@Unjis-MacBook-Pro.local>
2023-11-06 16:21:54 -0500

    finished project

---
+++ index.js
@@ -0,0 +1,20 @@
+//Create a message for the user of the combination
+//create a variable for the first combination  using division
+//create a variable for the second combination using multiplication
+//create a variable for the third combination using addtion 
+
+// Calculate the first code using division 
+const code1= 100 / 10;
+//Calculate the second code using multiplication
+const code2= 2 * 20;
+//Calculate the third code using addition
+const code3= 29 + 10;
+// Display the combination 
+
+
+//Message for the user with the combination 
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${code1} - ${code2} - ${code3}";
+
+//Display the message in file
+alert [welcome!]
+
